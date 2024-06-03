77132fb4fdf34c3156bb0a5dfe9a37924a2f53fd
fawwazmak <fawwazmakinde47@gmail.com>
2024-06-03 15:24:44 +0100

    new changes

--- src/Component/Working_proces.jsx
+++ src/Component/Working_proces.jsx
@@ -16,7 +16,31 @@ const CardComponent = () => {
     "Primary",
     "Primary",
   ];
-  const images = [first, second, third, fourth, fifth, sixth]; // Array of images
+  const images = [first, second, third, fourth, fifth, sixth];
+  const name = [
+    "John Smith",
+    "Jane Doe",
+    "Micheal Brawn",
+    "Emmily Johnson",
+    "Brian Williams",
+    "Sarah Kim",
+  ];
+  const title = [
+    "CEO and Founder",
+    "Directors of Operations",
+    "Senior SEO Specialist",
+    "PPC manager",
+    "Social Media Specialist",
+    "Content Creator",
+  ];
+  const testimonials = [
+    "10+ years of experience in digital marketing. Expertise in SEO, PPC, and content strategy",
+    "7+ years of experience in project management and team leadership. Strong organizational",
+    "5+ years of experience in SEO and content creation. Proficient in keyword research",
+    "3+ years of experience in paid search advertising. Skilled in campaign management and performance analysis",
+    "4+ years of experience in social media marketing. Proficient in creating content, analyzing metrics, and building engagement",
+    "2+ years of experience in writing and editing Skilled in creating compelling, SEO-optimized content for various industries",
+  ]; // Arrays
 
   return (
     <Container>
@@ -40,15 +64,19 @@ const CardComponent = () => {
                     alt={`Card image ${index + 1}`}
                     width="80px"
                   />
-                  <p>Header</p>
+                  <div className="align-self-end text-start">
+                    <p style={{ margin: 0, fontWeight: "bold" }}>
+                      {name[index]}
+                    </p>
+                    <p style={{ margin: 0 }}>{title[index]}</p>
+                  </div>
+                  <i class="fa-brands fa-linkedin-in bg-dark p-2 text-success rounded-circle"></i>
                 </div>
               </div>
               <hr />
               <Card.Body>
-                <Card.Title>{variant} Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                <Card.Text className="text-start">
+                  {testimonials[index]}
                 </Card.Text>
               </Card.Body>
             </Card>
