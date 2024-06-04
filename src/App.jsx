7bc3036cca091c4e47cb0a5ad79e5a4d6422cb95
black-myke <michaelbolajoko@gmail.com>
2024-06-04 15:59:59 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Services from "./components/Services.jsx";
+import Magnifier from "./assets/images/magnifier.png"
+import Mails from "./assets/images/mails.png"
+import Taskbar from "./assets/images/task-bar.png"
+import Tabpages from "./assets/images/tab-pages.png"
+import Chats from "./assets/images/chats.png"
+import Selectarrow from "./assets/images/select-arrow.png"
+import Blackarrow from "./assets/images/black-arrow.png"
+import Lemonarrow from "./assets/images/lemon-arrow.png"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ padding: "50px" }}>
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <Services
+          title1="Search engine"
+          title2="optimization"
+          arrow={Lemonarrow}
+          image={Magnifier}
+          bgColor={"#F3F3F3"}
+          arrowcol={"#191A23"}
+          bgText={"#B9FF66"}
+        />
+        <Services
+          title1="Pay-per-click"
+          title2="advertising"
+          arrow={Lemonarrow}
+          image={Selectarrow}
+          bgColor={"#B9FF66"}
+          arrowcol={"#191A23"}
+          bgText={"#ffffff"}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <Services
+          title1="Social Media"
+          title2="Marketing"
+          arrow={Blackarrow}
+          image={Chats}
+          bgColor={"#191A23"}
+          arrowcol={"#ffffff"}
+          bgText={"#ffffff"}
+        />
+        <Services
+          title1="Email"
+          title2="Marketing"
+          arrow={Lemonarrow}
+          image={Mails}
+          bgColor={"#F3F3F3"}
+          arrowcol={"#191A23"}
+          bgText={"#B9FF66"}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <Services
+          title1="Content"
+          title2="Creation"
+          arrow={Lemonarrow}
+          image={Taskbar}
+          bgColor={"#B9FF66"}
+          arrowcol={"#191A23"}
+          bgText={"#ffffff"}
+        />
+
+        <Services
+          title1="Analytics and"
+          title2="Tracking"
+          arrow={Blackarrow}
+          image={Tabpages}
+          bgColor={"#191A23"}
+          arrowcol={"#ffffff"}
+          bgText={"#B9FF66"}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App
