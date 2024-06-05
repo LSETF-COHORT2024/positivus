07304b6d0eee463deb5cf313c293b56a002b3e0e
Adams99Abubakry <adamsabubakrsiddique@gmail.com>
2024-06-05 09:51:09 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './assets/CSS/ImageGallerry.css'
+import image6 from './assets/images/image6.png';
+import image1 from './assets/images/image1.png';
+import image2 from './assets/images/image2.png';
+import image3 from './assets/images/image3.png';
+import image4 from './assets/images/image4.png';
+import image5 from './assets/images/image5.png';
+import Services from './components/service';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+    <div className="image-gallery">
+      <img src={image6} alt="Image 6" />
+      <img src={image1} alt="Image 1" />
+      <img src={image5} alt="Image 5" />
+      <img src={image4} alt="Image 4" />
+      <img src={image3} alt="Image 3" />
+      <img src={image2} alt="Image 2" /> 
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Services/>
+    </div>
+);
+};
 
 export default App
---
+++ src/components/service.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Services() {
+  return (
+    <div id="services">
+      <h3>Services</h3>
+      <h6>
+        At our digital marketing agency, we offer a range of services to
+        <br />
+        help businesses grow and succeed online. These services include:
+      </h6>
+    </div>
+  );
+}
+export default Services;
